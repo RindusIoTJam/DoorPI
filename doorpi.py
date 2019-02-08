7f7bd4cb65ee3e587977088e5e4ce9ca5bef3f31
Manfred Ackermann <manfred.ackermann@gmail.com>
2019-02-08 18:17:37 +0100

    issues/4 ignore open action if to timeout is active.

--- doorpi.py
+++ doorpi.py
@@ -211,17 +211,13 @@ class DoorSocketHandler(tornado.websocket.WebSocketHandler):
             # Extend the time if another ring occurs
             DoorSocketHandler.timeout_thread.extend()
 
-        try:
-            r = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2)) + \
-                "%s" % calendar.timegm(time.gmtime())
+        r = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2)) + \
+            "%s" % calendar.timegm(time.gmtime())
 
-            Application.config()['_door.open.random'] = r
+        Application.config()['_door.open.random'] = r
 
+        if Application.has_valid_slack_config(Application.config()):
             SlackHandler.send('@here DING DONG :: open >>> <%s/slack?%s|HERE> <<<' % (Application.config('slack.baseurl'), r))
-        except AttributeError:
-            pass
-        except KeyError:
-            pass
 
     @classmethod
     def handle_open(cls):
@@ -238,12 +234,8 @@ class DoorSocketHandler(tornado.websocket.WebSocketHandler):
         if not emulation:
             DoorSocketHandler.door.on()
 
-        try:
+        if Application.has_valid_slack_config(Application.config()):
             SlackHandler.send('@here DoorPI has opened the door.')
-        except AttributeError:
-            pass
-        except KeyError:
-            pass
 
         if not emulation:
             time.sleep(0.5)
