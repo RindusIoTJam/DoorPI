c17a107f885bcf600b14d70803231ca7c5b55db7
Manfred Ackermann <manfred.ackermann@gmail.com>
2019-03-07 01:13:02 +0100

    ServiceWorker generally working but goes to sleep after some time. Also JSlint is awesome.

--- static/index.js
+++ static/index.js
@@ -1,153 +1,164 @@
+/*jslint browser: true*/
+/*global $, WebSocket, Notification*/
+
 var audioElement;
 var audioReady = false;
 
-$(document).ready(function() {
+function prettyDate(utcSeconds) {
+    "use strict";
+    var d = new Date(0);
+    d.setUTCSeconds(utcSeconds);
+    return d.toString().substring(0, 24);
+}
+
+function notifyRing(date) {
+    "use strict";
+    if (Notification.permission === 'granted') {
+        // TODO: See https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register
+        navigator.serviceWorker.register('static/service-worker.js').then(function (registration) {
+            if (registration) {
+                var options = {
+                    body: 'Somebody rang ' + date + '!',
+                    vibrate: [100, 50, 100],
+                    actions: [
+                        {action: 'open', title: 'Open the door'},
+                    ]
+                };
+                registration.showNotification('Ring Ring!', options);
+            } else {
+                console.log("serviceWorker registration failed.");
+            }
+        });
+    }
+}
+
+var updater = {
+    socket: null,
+
+    start: function () {
+        "use strict";
+        var url = "wss://" + location.host + "/door";
+        if (location.protocol !== 'https:') {
+            url = "ws://" + location.host + "/door";
+        }
+        updater.secret = null;
+        updater.socket = new WebSocket(url);
+        updater.socket.onmessage = function (event) {
+            console.log(event.data);
+            var action = JSON.parse(event.data).action,
+                pretty = prettyDate(JSON.parse(event.data).timestamp);
+            if (action === "ring") {
+                updater.secret = JSON.parse(event.data).secret;
+                $('#open').removeAttr("disabled");
+                if (audioReady === true) {
+                    audioElement.play().catch(function (error) {
+                        console.log("INFO: play() failed because the user didn't interact " +
+                                    "with the document first. https://goo.gl/xX8pDD");
+                        console.log(error);
+                    });
+                }
+                notifyRing(pretty);
+                $('#last_ring').html(pretty);
+            }
+            if (action === "open") {
+                $('#open').prop("disabled", true);
+                $('#last_open').html(pretty);
+            }
+            if (action === "update") {
+                if (JSON.parse(event.data).last_open.length > 0) {
+                    $('#last_open').html(prettyDate(JSON.parse(event.data).last_open));
+                }
+                if (JSON.parse(event.data).last_ring.length > 0) {
+                    $('#last_ring').html(prettyDate(JSON.parse(event.data).last_ring));
+                }
+            }
+            if (action === 'timeout') {
+                $('#open').prop("disabled", true);
+            }
+        };
+        updater.socket.onopen = function () {
+            console.log("WS Connected");
+            $("#alert").hide();
+        };
+        // Reconnect function
+        updater.socket.onclose = function () {
+            console.log("WS Disconnected");
+            $("#alert").html("Lost connection to DoorPI, trying to reconnect.");
+            $("#alert").show();
+            setTimeout(function () {
+                var om = updater.socket.onmessage,
+                    oo = updater.socket.onopen,
+                    oc = updater.socket.onclose,
+                    wu = "wss://" + location.host + "/door";
+                if (location.protocol !== 'https:') {
+                    wu = "ws://" + location.host + "/door";
+                }
+                updater.socket = new WebSocket(wu);
+                updater.socket.onmessage = om;
+                updater.socket.onopen = oo;
+                updater.socket.onclose = oc;
+            }, 1000);
+        };
+    },
+};
+
+function send(message) {
+    "use strict";
+    console.log('sending', JSON.stringify(message));
+    updater.socket.send(JSON.stringify(message));
+}
+
+$(document).ready(function () {
+    "use strict";
     $('#open').prop("disabled", true);
 
-    if (!window.console) window.console = {};
-    if (!window.console.log) window.console.log = function() {};
+    if (!window.console) { window.console = { /* NOP*/ }; }
+    if (!window.console.log) { window.console.log = { /* NOP*/ }; }
 
-    $('button').click(function(event) {
-        if( event.target.id == "open" ) {
+    $('button').click(function (event) {
+        if (event.target.id === "open") {
             $('#open').prop("disabled", true);
             send({"action": event.target.id, "secret": updater.secret });
-        }
-        else {
+        } else {
             send({"action": event.target.id });
         }
         return false;
     });
 
-    setInterval(function() {
+    setInterval(function () {
         $('#time').html(new Date().toString().substring(0, 24));
     }, 1000);
 
     audioElement = document.createElement('audio');
     audioElement.setAttribute('src', '//' + location.host + '/static/ding-dong.mp3');
-    audioElement.addEventListener("canplay",function() {
+    audioElement.addEventListener("canplay", function () {
         audioReady = true;
     });
 
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('static/service-worker.js').then(function(registration) {
-          // Registration was successful
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-          // registration failed :(
-          console.log('ServiceWorker registration failed: ', err);
+    if (navigator.hasOwnProperty('serviceWorker')) {
+        navigator.serviceWorker.register('static/service-worker.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
         });
     }
 
-    Notification.requestPermission(function(status) {
+    Notification.requestPermission(function (status) {
         if (status === 'granted') {
-          console.log('Notification permission status:', status);
+            console.log('Notification permission status:', status);
         }
     });
 
     updater.start();
 });
 
-function notifyRing(date) {
-    if (Notification.permission == 'granted') {
-        navigator.serviceWorker.getRegistration('static/service-worker.js').then(function(registration) {
-            if(registration){
-                var options = {
-                    body: `Somebody rang ${date}!`,
-                    vibrate: [100, 50, 100],
-                    actions: [
-                       {action: 'open', title: 'Open the door'},
-                    ]
-                };
-                registration.showNotification('Ring Ring!', options);
-            }
-        });
-    }
-}
-
-if('serviceWorker' in navigator){
+if (navigator.hasOwnProperty('serviceWorker')) {
     // Handler for messages coming from the service worker
-    navigator.serviceWorker.addEventListener('message', function(event){
+    navigator.serviceWorker.addEventListener('message', function (event) {
+        "use strict";
         console.log("Client 1 Received Message: " + event.data);
         event.ports[0].postMessage("Client 1 Says 'Hello back!'");
     });
 }
-
-function send(message) {
-    console.log('sending', JSON.stringify(message));
-    updater.socket.send( JSON.stringify(message) );
-}
-
-function prettyDate(utcSeconds) {
-    var d = new Date(0);
-    d.setUTCSeconds(utcSeconds);
-    return d.toString().substring(0, 24);
-}
-
-var updater = {
-    socket: null,
-
-    start: function() {
-        if (location.protocol != 'https:') {
-            var url = "ws://" + location.host + "/door";
-        } else {
-            var url = "wss://" + location.host + "/door";
-        }
-        updater.secret = null
-        updater.socket = new WebSocket(url);
-        updater.socket.onmessage = function(event) {
-            console.log(event.data);
-            action=JSON.parse(event.data)['action'];
-            if( action == "ring" ) {
-                updater.secret = JSON.parse(event.data)['secret'];
-                $('#open').removeAttr("disabled");
-                if ( audioReady = true ) {
-                    audioElement.play().catch(function(error) {
-                        console.log("INFO: play() failed because the user didn't interact " +
-                                    "with the document first. https://goo.gl/xX8pDD");
-                    });
-                }
-                var pretty = prettyDate(JSON.parse(event.data)['timestamp']);
-                notifyRing(pretty);
-                $('#last_ring').html(pretty);
-            }
-            if( action == "open" ) {
-                $('#open').prop("disabled", true);
-                $('#last_open').html(prettyDate(JSON.parse(event.data)['timestamp']));
-            }
-            if( action == "update" ) {
-                if ( JSON.parse(event.data)['last_open'].length > 0 ) {
-                    $('#last_open').html(prettyDate(JSON.parse(event.data)['last_open']));
-                }
-                if ( JSON.parse(event.data)['last_ring'].length > 0 ) {
-                    $('#last_ring').html(prettyDate(JSON.parse(event.data)['last_ring']));
-                }
-            }
-            if( action == 'timeout' ) {
-                $('#open').prop("disabled", true);
-            }
-        }
-        updater.socket.onopen = function(event) {
-            console.log("WS Connected");
-            $("#alert").hide();
-        }
-        // Reconnect function
-        updater.socket.onclose = function(event) {
-            console.log("WS Disconnected");
-            $("#alert").html("Lost connection to DoorPI, trying to reconnect.")
-            $("#alert").show();
-            setTimeout(function() {
-                om=updater.socket.onmessage;
-                oo=updater.socket.onopen;
-                oc=updater.socket.onclose;
-                if (location.protocol != 'https:') {
-                    updater.socket = new WebSocket("ws://" + location.host + "/door");
-                } else {
-                    updater.socket = new WebSocket("wss://" + location.host + "/door");
-                }
-                updater.socket.onmessage=om;
-                updater.socket.onopen=oo;
-                updater.socket.onclose=oc;
-            }, 1000);
-        }
-    },
-};
--- static/service-worker.js
+++ static/service-worker.js
@@ -1,53 +1,55 @@
-self.addEventListener('notificationclick', function(event) {
-  var messageId = event.notification.data;
-
-  event.notification.close();
-
-  if (event.action === 'open') {
-    // clients.openWindow("/?open");
-    updater.socket.send( JSON.stringify(
-      {
-        "action": "open",
-        secret: updater.secret
-      }
-    ));
-  }
-}, false);
-
+/*jslint browser: true*/
+/*global self, WebSocket*/
 
 var updater = {
     socket: null,
 
-    start: function() {
-        if (location.protocol != 'https:') {
-            var url = "ws://" + location.host + "/door";
-        } else {
-            var url = "wss://" + location.host + "/door";
+    start: function () {
+        "use strict";
+        var action,
+            url = "wss://" + location.host + "/door";
+        if (location.protocol !== 'https:') {
+            url = "ws://" + location.host + "/door";
         }
-        updater.secret = null
+        updater.secret = null;
         updater.socket = new WebSocket(url);
-        updater.socket.onmessage = function(event) {
-            action=JSON.parse(event.data)['action'];
-            if( action == "ring" ) {
-              
-                updater.secret = JSON.parse(event.data)['secret'];
+        updater.socket.onmessage = function (event) {
+            action = JSON.parse(event.data).action;
+            if (action === "ring") {
+                updater.secret = JSON.parse(event.data).secret;
             }
-        }
+        };
         // Reconnect function
-        updater.socket.onclose = function(event) {
-            setTimeout(function() {
-                om=updater.socket.onmessage;
-                oc=updater.socket.onclose;
-                if (location.protocol != 'https:') {
+        updater.socket.onclose = function () {
+            setTimeout(function () {
+                var om = updater.socket.onmessage,
+                    oc = updater.socket.onclose;
+                if (location.protocol !== 'https:') {
                     updater.socket = new WebSocket("ws://" + location.host + "/door");
                 } else {
                     updater.socket = new WebSocket("wss://" + location.host + "/door");
                 }
-                updater.socket.onmessage=om;
-                updater.socket.onclose=oc;
+                updater.socket.onmessage = om;
+                updater.socket.onclose = oc;
             }, 1000);
-        }
+        };
     },
 };
+
+self.addEventListener('notificationclick', function (event) {
+    "use strict";
+    //var messageId = event.notification.data;
+    event.notification.close();
+
+    if (event.action === 'open') {
+        // clients.openWindow("/?open");
+        updater.socket.send(JSON.stringify(
+            {
+                "action": "open",
+                secret: updater.secret
+            }
+        ));
+    }
+}, false);
 
 updater.start();
